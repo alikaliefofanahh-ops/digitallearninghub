a6fc09c8fb1933a44feffddfd042d5a46c6b64ee
alikaliefofanahh-ops <alikaliefofanahh@gmail.com>
2025-11-16 15:00:32 +0000

    Add files via upload

---
+++ Dlh.js
@@ -0,0 +1,8 @@
+// Toggle menu on click
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
